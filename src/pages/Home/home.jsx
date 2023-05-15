70b30493836a9748d885bd5ffaa97a04131984c8
Sudipta <hisudipta26@gmail.com>
2023-05-15 23:23:36 +0530

    footer: newsletter, facilities, copyright added

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -12,7 +12,9 @@ const Home = () => {
         <div className="sales-content">
           <p>The Summer Sale</p>
           <p>Up to 30% off</p>
-          <button><span>Shop Now</span></button>
+          <button>
+            <span>Shop Now</span>
+          </button>
         </div>
       </div>
       <h2>First Purchase Offers</h2>
@@ -26,6 +28,60 @@ const Home = () => {
           <span className="code">USE CODE: FIRST200</span>
         </div>
       </div>
+      <footer>
+        <div className="newsletter">
+          <h2>Newsletter</h2>
+          <p>Get timely updates from your favorite products.</p>
+          <div className="input-container">
+            <input placeholder="Your email" />
+            <button>Send</button>
+          </div>
+        </div>
+        <div className="facilities">
+          <div>
+            <i class="fa-solid fa-truck-fast facilities-icon"></i>
+            <p>FREE SHIPPING</p>
+          </div>
+          <div>
+            <i class="fa-solid fa-rotate-left facilities-icon"></i>
+            <p>EASY RETURNS</p>
+          </div>
+          <div>
+            <i class="fa-solid fa-cloud-arrow-up facilities-icon"></i>
+            <p>100% Original Products</p>
+          </div>
+          <div>
+            <i class="fa-solid fa-tag facilities-icon"></i>
+            <p>Top Brands</p>
+          </div>
+          <div>
+            <p>Show some love on social media</p>
+            <div className="social-links">
+              <a href="https://github.com/SudiptaChakraborty51">
+                <i class="fa-brands fa-github fa-lg"></i>
+              </a>
+              <a href="https://twitter.com/Sudipta41158180">
+                <i class="fa-brands fa-twitter fa-lg"></i>
+              </a>
+              <a href="https://www.linkedin.com/in/sudipta-chakraborty-4a371a212/">
+                <i class="fa-brands fa-linkedin fa-lg"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="copyright">
+          <div>
+            <a href="/">Terms & Condition</a>
+            <a href="/">Shipping Policy</a>
+            <a href="/">Cancellation Policy</a>
+            <a href="/">Privacy Policy</a>
+          </div>
+          <p>
+            Copyright <i class="fa-regular fa-copyright"></i> 2023 Glamour. All
+            Rights Reserved.
+          </p>
+        </div>
+      </footer>
     </div>
   );
 };
