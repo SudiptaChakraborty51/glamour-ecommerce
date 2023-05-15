11c754d85f0aee86aa8f561fcdd7581ef24764d3
Sudipta <hisudipta26@gmail.com>
2023-05-15 21:15:23 +0530

    added offers card

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import Slider from "../../components/Slider/slider";
 import "./home.css";
+import background from "../../assets/background.png";
 
 const Home = () => {
   return (
     <div className="home">
       <h2>Best in Glamour</h2>
       <Slider />
+      <h2>First Purchase Offers</h2>
+      <div className="offers">
+        <div className="offer-card">
+            <p className="offer-text">FREE SHIPPING ON YOUR FIRST PURCHASE</p>
+            <span className="code">USE CODE: FIRSTGLAM</span>
+        </div>
+        <div className="offer-card">
+            <p className="offer-text">FLAT ₹200 OFF ON ORDERS ABOVE ₹2000</p>
+            <span className="code">USE CODE: FIRST200</span>
+        </div>
+      </div>
     </div>
   );
 };
