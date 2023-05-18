a60592a6ab2fd0f50297f9c93f150265006dbec0
Sudipta <hisudipta26@gmail.com>
2023-05-18 17:27:54 +0530

    fix: make a change in filtercontext

--- src/contexts/filterContext.jsx
+++ src/contexts/filterContext.jsx
@@ -1,9 +1,12 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useContext, useReducer } from "react";
 import filterReducer from "../reducer/filterReducer";
+import { ProductContext } from "./productContext";
 
 export const FilterContext = createContext();
 
 const FilterProvider = ({ children }) => {
+  const {productState} = useContext(ProductContext);
+  
   const initialFilter = {
     search: "",
     priceRange: 10000,
@@ -18,8 +21,68 @@ const FilterProvider = ({ children }) => {
     initialFilter
   );
 
+  console.log(".........................");
+  console.log("productState", productState);
+
+  const priceRangeFilteredProducts = productState?.products?.filter(
+    ({ price }) => Number(price) <= Number(filterState?.priceRange)
+  );
+
+  console.log("priceRangeProducts",priceRangeFilteredProducts);
+
+  const categoryFilteredProducts =
+    filterState?.categoryFilter?.length > 0
+      ? priceRangeFilteredProducts?.filter(({ categoryName }) =>
+          filterState?.categoryFilter?.includes(categoryName)
+        )
+      : priceRangeFilteredProducts;
+
+  console.log("categoryProducts", categoryFilteredProducts);
+
+  const brandFilteredProducts =
+    filterState?.brandFilter?.length > 0
+      ? categoryFilteredProducts?.filter(({ brand }) =>
+          filterState?.brandFilter?.includes(brand)
+        )
+      : categoryFilteredProducts;
+
+  console.log("brandProducts", brandFilteredProducts);
+
+  const ratingFilteredProducts =
+    filterState?.ratingFilter?.length > 0
+      ? brandFilteredProducts?.filter(
+          ({ ratings: { value } }) =>
+            Number(value) >= Number(filterState?.ratingFilter)
+        )
+      : brandFilteredProducts;
+
+  console.log("ratingProducts", ratingFilteredProducts);
+
+  const sortByPriceFilteredProducts =
+    filterState?.sortByPriceFilter?.length > 0
+      ? (() => {
+          switch (filterState.sortByPriceFilter) {
+            case "LTH":
+              return [...ratingFilteredProducts]?.sort(
+                (product1, product2) => product1.price - product2.price
+              );
+            case "HTL":
+              return [...ratingFilteredProducts]?.sort(
+                (product1, product2) => product2.price - product1.price
+              );
+            case "RESET":
+              return ratingFilteredProducts;
+            default:
+              return ratingFilteredProducts;
+          }
+        })()
+      : ratingFilteredProducts;
+
+  console.log("SortProducts", sortByPriceFilteredProducts);
+  console.log(".........................");
+
   return (
-    <FilterContext.Provider value={{ filterState, filterDispatch }}>
+    <FilterContext.Provider value={{ filterState, filterDispatch, sortByPriceFilteredProducts }}>
       {children}
     </FilterContext.Provider>
   );
--- src/pages/ProductListing/productListing.jsx
+++ src/pages/ProductListing/productListing.jsx
@@ -1,73 +1,12 @@
 import React, { useContext } from "react";
-import { ProductContext } from "../../contexts/productContext";
 import ProductCard from "../../components/ProductCard/productCard";
 import "./productListing.css";
 import Sidebar from "../../components/Sidebar/sidebar";
 import { FilterContext } from "../../contexts/filterContext";
 
 const ProductListing = () => {
-  const { productState } = useContext(ProductContext);
-  const { filterState } = useContext(FilterContext);
-  console.log(".........................");
-  console.log("productState", productState);
-
-  const priceRangeFilteredProducts = productState?.products?.filter(
-    ({ price }) => Number(price) <= Number(filterState?.priceRange)
-  );
-
-  console.log("priceRangeProducts",priceRangeFilteredProducts);
-
-  const categoryFilteredProducts =
-    filterState?.categoryFilter?.length > 0
-      ? priceRangeFilteredProducts?.filter(({ categoryName }) =>
-          filterState?.categoryFilter?.includes(categoryName)
-        )
-      : priceRangeFilteredProducts;
-
-  console.log("categoryProducts", categoryFilteredProducts);
-
-  const brandFilteredProducts =
-    filterState?.brandFilter?.length > 0
-      ? categoryFilteredProducts?.filter(({ brand }) =>
-          filterState?.brandFilter?.includes(brand)
-        )
-      : categoryFilteredProducts;
-
-  console.log("brandProducts", brandFilteredProducts);
-
-  const ratingFilteredProducts =
-    filterState?.ratingFilter?.length > 0
-      ? brandFilteredProducts?.filter(
-          ({ ratings: { value } }) =>
-            Number(value) >= Number(filterState?.ratingFilter)
-        )
-      : brandFilteredProducts;
-
-  console.log("ratingProducts", ratingFilteredProducts);
-
-  const sortByPriceFilteredProducts =
-    filterState?.sortByPriceFilter?.length > 0
-      ? (() => {
-          switch (filterState.sortByPriceFilter) {
-            case "LTH":
-              return [...ratingFilteredProducts]?.sort(
-                (product1, product2) => product1.price - product2.price
-              );
-            case "HTL":
-              return [...ratingFilteredProducts]?.sort(
-                (product1, product2) => product2.price - product1.price
-              );
-            case "RESET":
-              return ratingFilteredProducts;
-            default:
-              return ratingFilteredProducts;
-          }
-        })()
-      : ratingFilteredProducts;
-
-  console.log("SortProducts", sortByPriceFilteredProducts);
-  console.log(".........................");
-
+  const { sortByPriceFilteredProducts } = useContext(FilterContext);
+  
   return (
     <div className="products">
       <div className="sidebar-filter">
