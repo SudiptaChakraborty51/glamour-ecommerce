b0816ae00418200697653ce2a5dcb806bd2373a9
Sudipta <hisudipta26@gmail.com>
2023-05-22 00:43:28 +0530

    wishlist implemented

--- src/pages/Wishlist/wishlist.jsx
+++ src/pages/Wishlist/wishlist.jsx
@@ -2,15 +2,26 @@ import React, { useContext } from "react";
 import "./wishlist.css";
 import { ProductContext } from "../../contexts/productContext";
 import EmptyWishlist from "./emptyWishlist";
+import ProductCard from "../../components/ProductCard/productCard";
 
 const Wishlist = () => {
   const { productState } = useContext(ProductContext);
   console.log(productState?.wishlist);
-  return (
-      productState?.wishlist?.length === 0 ? <EmptyWishlist /> :
-      <div className="wishlist">
-        <h1>Wishlist ({productState?.wishlist?.length})</h1>
+  return productState?.wishlist?.length === 0 ? (
+    <EmptyWishlist />
+  ) : (
+    <div className="wishlist">
+      <h1>Wishlist ({productState?.wishlist?.length})</h1>
+      <div className="wishlist-container">
+        <ul>
+          {productState?.wishlist?.map((product) => (
+            <li>
+              <ProductCard productsData={product} key={product._id} />
+            </li>
+          ))}
+        </ul>
       </div>
+    </div>
   );
 };
 
