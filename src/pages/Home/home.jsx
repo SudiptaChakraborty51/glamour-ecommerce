2d67429568f8c19d0e70f7a0b60eaa1bff02b9da
Sudipta <hisudipta26@gmail.com>
2023-05-30 16:17:47 +0530

    fix: responsive for bestseller products

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -17,15 +17,19 @@ const Home = () => {
       items: 5,
     },
     desktop: {
-      breakpoint: { max: 3000, min: 1024 },
+      breakpoint: { max: 3000, min: 1380 },
+      items: 4,
+    },
+    desktop: {
+      breakpoint: { max: 1380, min: 1024 },
       items: 3,
     },
     tablet: {
-      breakpoint: { max: 1024, min: 464 },
+      breakpoint: { max: 1024, min: 684 },
       items: 2,
     },
     mobile: {
-      breakpoint: { max: 464, min: 0 },
+      breakpoint: { max: 684, min: 0 },
       items: 1,
     },
   };
